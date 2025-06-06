adbefa9153ad943e13c4be1d65d25def339be284
AdityaPatadiya <patadiyaaditya0079@gmail.com>
2025-06-06 15:12:26 +0000

    bug fixing.

--- healthcare_backend/api/views.py
+++ healthcare_backend/api/views.py
@@ -1,13 +1,15 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
-from rest_framework.permissions import IsAuthenticated
+from rest_framework.permissions import IsAuthenticated, AllowAny
 from rest_framework_simplejwt.tokens import RefreshToken
 from .models import Patient, Doctor, PatientDoctorMapping
 from .serializers import PatientSerializer, RegisterSerializer, DoctorSerializer, PatientDoctorMappingSerializer
 
 
 class RegisterView(APIView):
+    permission_classes = [AllowAny]
+
     def post(self, request):
         serializer = RegisterSerializer(data=request.data)
         if serializer.is_valid():

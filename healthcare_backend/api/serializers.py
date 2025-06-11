93282aa6fe2658b7986dc0d749952448cfa1cc0b
AdityaPatadiya <patadiyaaditya0079@gmail.com>
2025-06-11 11:13:57 +0000

    bug fixed.

--- healthcare_backend/api/serializers.py
+++ healthcare_backend/api/serializers.py
@@ -4,7 +4,7 @@ from django.contrib.auth.models import User
 
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
-        models = User
+        model = User
         fields = ['id', 'username', 'email']
 
 
